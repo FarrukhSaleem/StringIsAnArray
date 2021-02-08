4c0040f29ed6cd058988e2122ebd8d649c542bbe
Farrukh Saleem Sheikh <farrukhsaleem_01@yahoo.com>
2021-02-08 06:44:20 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,9 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    string a ="sdjhsjkdfhsdjkfh";
+    for (int i=0;i<a.Length;i++)
+    Console.WriteLine(a[i]);
+  }
+}
